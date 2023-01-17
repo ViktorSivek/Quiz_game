03b7adb1ad75827fbca790b16bff0361fece39c5
Viktor Sívek <sivv01@vse.cz>
2023-01-17 20:41:38 +0100

    01

--- style/js/admin.js
+++ style/js/admin.js
@@ -1,2 +1,29 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('question-text'));
+const questionsList = document.getElementById("questionsList");
+const Questions = JSON.parse(localStorage.getItem("Questions")) || [];
+
+console.log(Questions)
+console.log(questionsList)
+questionsList.innerHTML = Questions
+    .map(newQuestion => {
+        return `
+        <h2 class="text-center">${newQuestion.newQuestion}</h2>
+        <div class="question-container">
+            <p class="question-prefix">A</p>
+            <p class="question-text">${newQuestion.choice1}</p>
+        </div>
+        <div class="question-container">
+            <p class="question-prefix">B</p>
+            <p class="question-text">${newQuestion.choice2}</p>
+        </div>
+        <div class="question-container">
+            <p class="question-prefix">C</p>
+            <p class="question-text">${newQuestion.choice3}</p>
+        </div>
+        <div class="question-container">
+            <p class="question-prefix">D</p>
+            <p class="question-text">${newQuestion.choice4}</p>
+        </div>
+        
+        <p class="new-Question">Correct Answer - ${newQuestion.answer}</p>`;
+    })
+    .join("");
